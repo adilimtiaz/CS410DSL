dedf23d22553819f713eacdcc92bb9c077cb34bc
Raymond Zeng <rzeng@sfu.ca>
2018-10-17 04:45:29 -0700

    Added Some Test Files

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require("path");
 const generators = require("./lib/generators");
 const mongoHandler = require('./lib/mongoHandler');
 
-let inputText = fs.readFileSync(path.join(__dirname ,'./GrammarSamples/NoEnd.txt'), 'utf8');
+let inputText = fs.readFileSync(path.join(__dirname ,'./GrammarSamples/InvalidExamples/InvalidSample3.txt'), 'utf8');
 
 let programAst = toAstVisitor(inputText);
 
